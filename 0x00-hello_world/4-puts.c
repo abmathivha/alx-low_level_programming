43152bf7c6336886d55c8fecb945ce7bef9154b5
abmathivha <mathstgg@gmail.com>
2023-07-13 07:48:24 -0700

    Adding 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints a statement using puts, and goes to a new line
+ * Return: 0 always
+ */
+#include<stdio.h> /* for standard output with puts */
+int main(void)
+{
+	/* Outputing a message and a new line */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
