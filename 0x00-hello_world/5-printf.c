3db1286389780f5e146f888d0518fdc54d44c049
abmathivha <mathstgg@gmail.com>
2023-07-13 07:51:58 -0700

    Adding 5-pfintf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints a statement using puts, and goes to a new line
+ * Return: 0 always
+ */
+#include<stdio.h> /* for standard output with puts */
+
+int main(void)
+{
+	/* Outputing a message and a new line */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
