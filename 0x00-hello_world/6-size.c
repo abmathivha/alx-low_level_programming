06e8a676e0fc155fbbfab720d9f46ab0eda19c25
abmathivha <mathstgg@gmail.com>
2023-07-13 09:23:59 -0700

    Adding 6-sizes.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+/**
+ * main - prints sizes of different data types
+ *
+ * Return: 0 if all goes well else a non zero
+ */
+#include<stdio.h> /* for standard output  */
+
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+}
