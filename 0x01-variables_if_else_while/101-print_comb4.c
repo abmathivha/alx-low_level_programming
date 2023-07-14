dee59b572d8fc302b4886e8fb9043d946cc7ff0f
abmathivha <mathstgg@gmail.com>
2023-07-14 14:41:13 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets, a to z.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j;
+	int k;
+
+	while (i <= 55)
+	{
+		j = i + 1;
+		while (j <= 56)
+		{       	
+			k = j + 1;
+			while (k <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i + j + k != 168)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;	
+	}
+	putchar('\n');
+	return (0);
+}
