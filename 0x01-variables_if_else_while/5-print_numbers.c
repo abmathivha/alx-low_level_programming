6f000956196d841f3966f039d2eb5b55f18ffa32
abmathivha <mathstgg@gmail.com>
2023-07-14 13:20:41 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets, a to z.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	numbers = "0123456789";
+	int i = 0;
+
+	while (i <= 8)
+	{
+		putchar(numbers[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
