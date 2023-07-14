1a8e7d4a4b18966c2c6187e5de3defa9a4994d15
abmathivha <mathstgg@gmail.com>
2023-07-14 12:42:20 -0700

    Adding 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets, a to z.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+	
+	while(i <= 25)
+	{
+		putchar(letters[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
