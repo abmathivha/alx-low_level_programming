2d40e17487007870c45a83eb6042e00c4fbc4415
abmathivha <mathstgg@gmail.com>
2023-07-18 12:25:40 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+/**
+ * print_alphabet - prints a to z
+ *
+ * Description: prints a to z and a new line
+ *
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
