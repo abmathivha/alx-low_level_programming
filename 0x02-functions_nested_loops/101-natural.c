6a1cf8bd44337bdc0eca157df5bada28fbfc89ab
abmathivha <mathstgg@gmail.com>
2023-07-18 16:11:37 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - computes and prints the sum of all the
+ **/
+int main(void)
+{
+	int sum, i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (a % 3 == 0 || a % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
