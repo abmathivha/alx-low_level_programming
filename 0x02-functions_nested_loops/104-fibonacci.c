98695ad5d9df318006febf9a78f7919a1fd2cb1e
abmathivha <mathstgg@gmail.com>
2023-07-18 18:28:45 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,13 +9,13 @@ int main(void)
 	float prev = 1, cur = 2, next = 0;
 	int i;
 
-	for (i = 0; i < 88; i++)
+	for (i = 0; i < 84; i++)
 	{
 		printf("%.0f, ", prev);
 		next  = cur + prev;
 		prev = cur;
 		cur = next;
-		if (i == 87)
+		if (i == 83)
 			printf("%.0f\n", prev);
 	}
 	return (0);
