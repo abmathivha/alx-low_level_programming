2231ba2f5a33734f1a017522975225744792e5cc
abmathivha <mathstgg@gmail.com>
2023-07-18 17:35:56 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,9 +19,9 @@ void print_times_table(int n)
 		{
 			if (j != 0)
 			{
-				printf(", ");
+				printf(",   ");
 			}
-			printf("%-2d", i * j);
+			printf("%4d", i * j);
 		}
 		printf("\n");
 	}
