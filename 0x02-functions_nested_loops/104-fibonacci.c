31836a820d76a7c1dac50d3cd7296c1e8d1476dc
abmathivha <mathstgg@gmail.com>
2023-07-20 14:39:48 -0700

    Removed a dir

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
-#include <inttypes.h>
-/**
- * main - print first 50 fibonacci
- *
- * Return: 0 always.
- */
+
+#define LARGEST 10000000000
+
 int main(void)
 {
-	unsigned __int128 prev = 1, cur = 2, next = 0;
-	int i;
+    unsigned long int prevFib1 = 0, currentFib1 = 1, prevFib2 = 0, currentFib2 = 2;
+    unsigned long int carry1, carry2, sum;
+    int count;
 
-	for (i = 0; i < 97; i++)
-	{
-		printf("%llu, ",(unsigned long long) prev);
-		next  = cur + prev;
-		prev = cur;
-		cur = next;
-		if (i == 96)
-			printf("%llu\n",(unsigned long long)prev);
-	}
-	return (0);
+    printf("%lu, %lu, ", currentFib1, currentFib2);
+
+    for (count = 2; count < 98; count++)
+    {
+        if (currentFib1 + currentFib2 > LARGEST || prevFib2 > 0 || prevFib1 > 0)
+        {
+            carry1 = (currentFib1 + currentFib2) / LARGEST;
+            carry2 = (currentFib1 + currentFib2) % LARGEST;
+            sum = prevFib1 + prevFib2 + carry1;
+            prevFib1 = prevFib2;
+            prevFib2 = sum;
+            currentFib1 = currentFib2;
+            currentFib2 = carry2;
+            printf("%lu%010lu", prevFib2, currentFib2);
+        }
+        else
+        {
+            carry2 = currentFib1 + currentFib2;
+            prevFib1 = currentFib1;
+            prevFib2 = currentFib2;
+            currentFib1 = currentFib2;
+            currentFib2 = carry2;
+            printf("%lu", currentFib2);
+        }
+
+        if (count != 97)
+            printf(", ");
+    }
+
+    printf("\n");
+    return 0;
 }
