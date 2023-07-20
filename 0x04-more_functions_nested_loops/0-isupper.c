929e8b6d4c5ac15f9d35ead0361ac7efd92ced5c
abmathivha <mathstgg@gmail.com>
2023-07-20 15:05:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - prints a to z ten times
+ *
+ * Description: determines if a char is lower
+ * @c: the input char
+ *
+ * Return: 1 if char is lower
+*/
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')i
+	{
+		return (1);  /* Character is lowercase */
+	}
+	return (0);  /* Character is not lowercase */
+}
