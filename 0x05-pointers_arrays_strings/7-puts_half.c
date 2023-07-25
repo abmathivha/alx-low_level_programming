7e6fd016bada7e4b5bec6cd469db8c46776b4853
abmathivha <mathstgg@gmail.com>
2023-07-24 17:49:21 -0700

    Adding task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * puts_half - print last half of a string
+  *
+  * @s : a string pointer
+  * Return: nothing
+  */
+void puts_half(char *s)
+{
+	int length = 0;
+	int i = 0;
+	char temp;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	s--;
+	s -= (length / 2);
+
+	while (*s != '\0')
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
