59676588c9c035828828847b12405687b9f0c2bb
abmathivha <mathstgg@gmail.com>
2023-07-24 18:18:10 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_array - print first n elements of an array
+  *
+  * @a : an int pointer
+  * @n : number of elements
+  * Return: nothing
+  */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d, ", *a);
+		i++;
+	}
+	printf("\n");
+}
