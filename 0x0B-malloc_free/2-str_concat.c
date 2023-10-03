bdfb1968e115ef2aeda75b86894134bf7daeda4b
abmathivha <mathstgg@gmail.com>
2023-10-03 10:45:42 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 
 /**
- * str_concat - Concatenates two strings.
- * @s1: The string to be concatenated upon.
- * @s2: The string to be concatenated to s1.
+ * str_concat - concatenates two strings.
+ * @str1: The string to be concatenated upon.
+ * @str2: The string to be concatenated to str1.
  *
  * Return: If concatenation fails - NULL.
  *  Otherwise - a pointer the newly-allocated space in memory
  *  containing the concatenated strings
  */
 
-char *str_concat(char *s1, char *s2)
+char *str_concat(char *str1, char *str2)
 {
 	char *concat;
 	unsigned int i;
 	unsigned int j;
-	unsigned int length_S1;
-	unsigned int length_S2;
-	unsigned int length_c;
+	unsigned int len_str1;
+	unsigned int len_str2;
+	unsigned int len_c;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (str1 == NULL)
+		str1 = "";
+	if (str2 == NULL)
+		str2 = "";
 
-	for (length_S1 = 0; s1[length_S1] != '\0'; length_S1++)
+	for (len_str1 = 0; str1[len_str1] != '\0'; len_str1++)
 		;
 
-	for (length_S2 = 0; s2[length_S2] != '\0'; length_S2++)
+	for (len_str2 = 0; str2[len_str2] != '\0'; len_str2++)
 		;
 
-	length_c = length_S1 + length_S2;
+	len_c = len_str1 + len_str2;
 
-	concat = malloc((length_c + 1) * sizeof(char));
+	concat = malloc((len_c + 1) * sizeof(char));
 	if (concat == NULL)
 		return (NULL);
 
-	for (i = 0; i < length_S1; i++)
-		concat[i] = s1[i];
+	for (i = 0; i < len_str1; i++)
+		concat[i] = str1[i];
 
-	for (j = 0; j < length_S2; i++, j++)
-		concat[i] = s2[j];
+	for (j = 0; j < len_str2; i++, j++)
+		concat[i] = str2[j];
 
 	return (concat);
 }
