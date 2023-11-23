72c13d14de7cb8d82ff5f20a0c3645a1739da8d3
abmathivha <mathstgg@gmail.com>
2023-11-23 05:22:28 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,7 +12,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int m;
 
-	if (index > sizeof(unsigned long int) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
 	m = 1 << index;
